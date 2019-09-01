c5172499e692081e4964abe1dab50ac6d45cc48a
Phoenix Project <phoenixproject.erp@gmail.com>
2019-09-01 13:14:55 -0300

    Net Connect PHP finalizado, testado e comentado

--- _NETCONNECTPHP/app/src/main/java/br/com/codelogic/netconnectphp/MainActivity.kt
+++ _NETCONNECTPHP/app/src/main/java/br/com/codelogic/netconnectphp/MainActivity.kt
@@ -15,17 +15,27 @@ import java.io.*
 import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.R.attr.data
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
 
+    // Termos que levarão o nome da url ( (se requisição for via GET ou POST)
+    // e o conteúdo de dados em JSON  (se requisição for via POST)
     val termos = arrayOfNulls<String>(2);
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //val termos = arrayOfNulls<String>(2);
-
+        // URLs para teste de requisição
         //termos[0] = "http://192.168.1.49/contactsbkp/public/index.php";
         termos[0] = "http://192.168.1.113/php/contactsbkp/public/index.php";
         //termos[0] = "http://192.168.1.113/presidentslist2.json"
@@ -33,35 +43,34 @@ class MainActivity : AppCompatActivity() {
         //termos[0] = "http://codelogic.com.br/presidentslist.json";
         //termos[0] = "http://codelogic.com.br/presidentslist2.json";
 
+        // Tipo de objeto que será enviado ao servidor
+        // (só utilizado se a requisição for via POST)
         var ot = TesteObjeto(7)
+        // List contendo objetos do tipo TesteObjeto para serem enviados ao servidor
+        // (só utilizado se a  requisição for via POST)
         var otlist: MutableList<TesteObjeto> = mutableListOf(ot)
 
+        // Gera conteúdo JSON para ser enviado ao servidor (só utilizado se a requisição for via POST)
         termos[1] = GerarJSON(otlist)
 
-        AsyncTaskHandleJsonGET().execute(termos[0])
+        // Tipos de requisição disponíveis
+        //AsyncTaskHandleJsonGET().execute(termos[0])
         //AsyncTaskHandleJsonPOST().execute(termos[0],termos[1]);
         //AsyncTaskHandleJsonPOST2().execute(termos[0],termos[1]);
+        //AsyncTaskHandleJsonPOST3().execute(termos[0],termos[1]);
+        //AsyncTaskHandleJsonPOST4().execute(termos[0],termos[1]);
     }
 
+    // Exemplo de Requisição via POST
     inner class AsyncTaskHandleJsonPOST : AsyncTask<String, String, String>() {
-        override fun doInBackground(vararg url: String?): String {
+        override fun doInBackground(vararg dado: String?): String {
 
-            //var method: String = "teste/testepost/"
-            var method: String = "api/values/"
-
-            //var url: URL? = null
-            //var url: URL? = URL("http://localhost:52522/" + method)
-            //var conexao: HttpURLConnection? = null
-            val conexao = URL(url[0]).openConnection() as HttpURLConnection
+            val conexao = URL(dado[0]).openConnection() as HttpURLConnection
             var json = ""
 
             val stringBuilder = StringBuilder()
 
             try {
-                //var urldeconexao = URL(url[0])
-
-                //conexao = urldeconexao.openConnection() as HttpURLConnection
-
                 conexao.readTimeout = 30000
                 conexao.connectTimeout = 30000
                 conexao.requestMethod = "POST"
@@ -77,8 +86,7 @@ class MainActivity : AppCompatActivity() {
                 val outputStream = BufferedOutputStream(conexao.outputStream)
                 val writer = BufferedWriter(OutputStreamWriter(outputStream, "utf-8"))
 
-                //writer.write(data)
-                writer.write(url[1])
+                writer.write(dado[1])
                 writer.flush()
                 writer.close()
 
@@ -98,7 +106,7 @@ class MainActivity : AppCompatActivity() {
                 val bufferedReader = BufferedReader(InputStreamReader(inputStream))
 
                 var line: String? = null
-                //while ((line = bufferedReader.readLine()) != null) {
+
                 while ((line == bufferedReader.readLine()) != null) {
                     stringBuilder.append(line!! + "\n")
                 }
@@ -113,24 +121,22 @@ class MainActivity : AppCompatActivity() {
                 conexao.disconnect()
             }
 
-            var teste : String = json
-
             return json
         }
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-            //handleJsonGET(result)
             handleJsonPOST(result)
         }
 
     }
 
+    // Exemplo de Requisição via POST
     inner class AsyncTaskHandleJsonPOST2 : AsyncTask<String,String,String>() {
-        override fun doInBackground(vararg url: String): String {
+        override fun doInBackground(vararg dado: String): String {
 
-            var endererourl : String = url[0]
-            var dados : String = url[1]
+            var endererourl : String = dado[0]
+            var dados : String = dado[1]
             var json : String = ""
 
             val stringBuilder = StringBuilder()
@@ -155,8 +161,6 @@ class MainActivity : AppCompatActivity() {
                 outputStream.write(postData)
                 outputStream.flush()
 
-                //outputStream.close()
-
             } catch (e: NullPointerException) {
                 e.printStackTrace()
             } catch (e: Exception) {
@@ -169,10 +173,9 @@ class MainActivity : AppCompatActivity() {
                     val inputStream: InputStream? = null
 
                     val reader: BufferedReader = BufferedReader(InputStreamReader(inputStream))
-                    //val output: String = reader.readLine()
+
                     json = reader.readLine()
 
-                    //println("There was error while connecting the chat $output")
                     println("There was error while connecting the chat $json")
                     System.exit(0)
 
@@ -181,29 +184,8 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            /*val jsonObject = JSONObject()
-            val inputStream: InputStream
-
-            // get stream
-            if (connection.responseCode < HttpURLConnection.HTTP_BAD_REQUEST) {
-                inputStream = connection.inputStream
-            } else {
-                inputStream = connection.errorStream
-            }
-
-            // parse stream
-            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-
-            var line: String? = null
-            //while ((line = bufferedReader.readLine()) != null) {
-            while ((line == bufferedReader.readLine()) != null) {
-                stringBuilder.append(line!! + "\n")
-            }
-
-            json = stringBuilder.toString()*/
             json = connection.inputStream.use { it.reader().use{reader -> reader.readText()} }
 
-            //return text
             return json
         }
 
@@ -214,11 +196,12 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // Exemplo de Requisição via POST
     inner class AsyncTaskHandleJsonPOST3 : AsyncTask<String,String,String>() {
-        override fun doInBackground(vararg url: String): String {
+        override fun doInBackground(vararg dado: String): String {
 
-            var endererourl : String = url[0]
-            var dados : String = url[1]
+            var endererourl : String = dado[0]
+            var dados : String = dado[1]
             var json : String = ""
 
             val stringBuilder = StringBuilder()
@@ -226,6 +209,7 @@ class MainActivity : AppCompatActivity() {
             val serverURL: String = endererourl
             val url = URL(serverURL)
             val connection = url.openConnection() as HttpURLConnection
+
             connection.requestMethod = "POST"
             connection.connectTimeout = 300000
             connection.connectTimeout = 300000
@@ -234,16 +218,15 @@ class MainActivity : AppCompatActivity() {
             val postData: ByteArray = dados.toByteArray(Charset.defaultCharset())
 
             connection.setRequestProperty("charset", "utf-8")
-            connection.setRequestProperty("Content-lenght", postData.size.toString())
+            connection.setRequestProperty("Content-length", postData.size.toString())
             connection.setRequestProperty("Content-Type", "application/json")
 
             try {
-
                 val outputStream: DataOutputStream = DataOutputStream(connection.outputStream)
                 outputStream.write(postData)
                 outputStream.flush()
 
-                //outputStream.close()
+                json = connection.inputStream.use { it.reader().use{reader -> reader.readText()} }
 
             } catch (e: NullPointerException) {
                 e.printStackTrace()
@@ -257,41 +240,19 @@ class MainActivity : AppCompatActivity() {
                     val inputStream: InputStream? = null
 
                     val reader: BufferedReader = BufferedReader(InputStreamReader(inputStream))
-                    //val output: String = reader.readLine()
+
                     json = reader.readLine()
 
-                    //println("There was error while connecting the chat $output")
                     println("There was error while connecting the chat $json")
                     System.exit(0)
 
                 } catch (exception: Exception) {
                     throw Exception("Exception while push the notification  $exception.message")
+                } finally {
+                    connection.disconnect()
                 }
             }
 
-            /*val jsonObject = JSONObject()
-            val inputStream: InputStream
-
-            // get stream
-            if (connection.responseCode < HttpURLConnection.HTTP_BAD_REQUEST) {
-                inputStream = connection.inputStream
-            } else {
-                inputStream = connection.errorStream
-            }
-
-            // parse stream
-            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-
-            var line: String? = null
-            //while ((line = bufferedReader.readLine()) != null) {
-            while ((line == bufferedReader.readLine()) != null) {
-                stringBuilder.append(line!! + "\n")
-            }
-
-            json = stringBuilder.toString()*/
-            json = connection.inputStream.use { it.reader().use{reader -> reader.readText()} }
-
-            //return text
             return json
         }
 
@@ -302,11 +263,69 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // Exemplo de Requisição via POST
+    inner class AsyncTaskHandleJsonPOST4 : AsyncTask<String,String,String>() {
+        override fun doInBackground(vararg dado: String): String {
+
+            var endererourl : String = dado[0]
+            var dados : String = URLEncoder.encode(dado[1], "UTF-8")
+            var json : String = ""
+
+            val stringBuilder = StringBuilder()
+
+            val serverURL: String = endererourl
+            val url = URL(serverURL)
+            val conexao = url.openConnection() as HttpURLConnection
+
+            with(url.openConnection() as HttpURLConnection) {
+                // optional default is GET
+                requestMethod = "POST"
+
+                readTimeout = 15000
+                connectTimeout = 15000
+
+                setRequestProperty("charset", "utf-8")
+                setRequestProperty("Content-Type", "application/json")
+                setRequestProperty("Accept", "application/json")
+
+                val wr = OutputStreamWriter(getOutputStream());
+                wr.write(dados);
+                wr.flush();
+
+                println("URL : $url")
+                println("Response Code : $responseCode")
+
+                BufferedReader(InputStreamReader(inputStream)).use {
+                    val response = StringBuffer()
+
+                    var inputLine = it.readLine()
+                    while (inputLine != null) {
+                        response.append(inputLine)
+                        inputLine = it.readLine()
+                    }
+                    it.close()
+                    println("Response : $response")
+
+                    json = response.toString()
+                }
+            }
+
+            return json
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            handleJsonPOST(result)
+        }
+
+    }
+
+    // Exemplo de Requisição via GET
     inner class AsyncTaskHandleJsonGET : AsyncTask<String, String, String>() {
-        override fun doInBackground(vararg url: String?): String {
+        override fun doInBackground(vararg dado: String?): String {
 
             var text: String
-            val connection = URL(url[0]).openConnection() as HttpURLConnection
+            val connection = URL(dado[0]).openConnection() as HttpURLConnection
 
             try{
                 connection.connect()
@@ -329,6 +348,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Tratamento de resposta do servidor para requisição via GET
     private fun handleJsonGET(jsonString: String?) {
 
         val jsonArray = JSONArray(jsonString)
@@ -354,9 +374,8 @@ class MainActivity : AppCompatActivity() {
         presidents_list.adapter = adapter
     }
 
+    // Tratamento de resposta do servidor para requisição via POST
     private fun handleJsonPOST(jsonString: String?) {
-
-        val teste = jsonString
 
         val jsonArray = JSONArray(jsonString)
 
@@ -379,10 +398,9 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = ListAdapter(this, list)
         presidents_list.adapter = adapter
-
-        //AsyncTaskHandleJsonPOST2().execute(termos[0],termos[1]);
     }
 
+    // Gera uma Array JSON a partir de uma lista de TipoObjeto
     fun GerarJSON(list: List<TesteObjeto>): String {
 
         //Inicializa um novo objeto Json
