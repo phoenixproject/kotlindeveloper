6618c838b452e56f04b23e4c5148cb3f9aa424bf
Phoenix Project <phoenixproject.erp@gmail.com>
2024-06-11 08:09:15 -0300

    Aplicativos Tip Calculator e Welcome adicionados

---
+++ _IFESHOMEWORK4/app/src/main/java/br/edu/ifes/ifeshomework4/Activity2.kt
@@ -0,0 +1,34 @@
+package br.edu.ifes.ifeshomework4
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.ComponentActivity
+
+class Activity2 : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+
+    fun clickActivityMainA2(view: View) {
+
+        val i: Intent = Intent(
+            this@Activity2,
+            MainActivity::class.java
+        )
+        startActivity(i)
+    }
+
+    fun clickActivity3A2(view: View) {
+
+        val i: Intent = Intent(
+            this@Activity2,
+            Activity3::class.java
+        )
+        startActivity(i)
+    }
+
+}
---
+++ _IFESHOMEWORK4/app/src/main/java/br/edu/ifes/ifeshomework4/MainActivity.kt
@@ -0,0 +1,67 @@
+package br.edu.ifes.ifeshomework4
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.edu.ifes.ifeshomework4.ui.theme.IFESHOMEWORK2Theme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*setContent {
+            IFESHOMEWORK2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }*/
+    }
+
+    fun clickActivity2(view: View) {
+        //Log.e("clickItemHoro", view!!.tag.toString())
+
+        val i: Intent = Intent(
+            this@MainActivity,
+            Activity2::class.java
+        )
+        startActivity(i)
+    }
+
+    fun clickActivity3(view: View) {
+
+        val i: Intent = Intent(
+            this@MainActivity,
+            Activity3::class.java
+        )
+        startActivity(i)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IFESHOMEWORK2Theme {
+        Greeting("Android")
+    }
+}
